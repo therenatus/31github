cbe3dcc17e02b50d19693a830d71980de2411e93
therenatus <therinatus@gmail.com>
2023-12-03 18:39:42 +0600

    day 6 "Power of Three"

--- 6day.ts
+++ 6day.ts
@@ -1,6 +1,6 @@
 function isPowerOfThree(n: number): boolean {
   if(n <= 0) return false;
   if (n === 1) return true;
-  if(n%3 === 0) return isPowerOfThree(n / 3);
+  if(n % 3 === 0) return isPowerOfThree(n / 3);
   return false;
 }
