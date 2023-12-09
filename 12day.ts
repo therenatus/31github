3c27e4e9f5aba59394faae4c898ee27a3fa1694e
therenatus <therinatus@gmail.com>
2023-12-09 23:44:57 +0600

    day 12 "Least Bricks"

---
+++ 12day.ts
@@ -0,0 +1,15 @@
+const leastBricks = (wall: number[][]): number => {
+  const map: { [key: number]: number } = {};
+  let max = 0;
+
+  wall.forEach((row) => {
+    let sum = 0;
+    for (let n = 0; n < row.length - 1; n++) {
+      sum += row[n];
+      map[sum] = map[sum] ? map[sum] + 1 : 1;
+      max = Math.max(map[sum], max);
+    }
+  });
+
+  return wall.length - max;
+};
